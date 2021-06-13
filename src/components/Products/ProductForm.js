d77d7176ab1d674521040f3cc66c6d0b736688c5
alaz7622356694 <alireza.azari2235@outlook.com>
2021-06-13 17:39:12 +0430

    adding second value to the onChange as initial values

--- src/components/Products/ProductForm.js
+++ src/components/Products/ProductForm.js
@@ -18,11 +18,11 @@ const inputState = useState({title:'' ,amount:''})
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Title</label>
-            <input type="text" id="title" value={inputState[0].title} onChange={(event)=>inputState[1]({title:event.target.value})}  />
+            <input type="text" id="title" value={inputState[0].title} onChange={(event)=>inputState[1]({title:event.target.value , amount:inputState[0].amount})}  />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" value={inputState[0].amount} onChange={(event)=>inputState[1]({amount:event.target.value})}/>
+            <input type="number" id="amount" value={inputState[0].amount} onChange={(event)=>inputState[1]({amount:event.target.value , title:inputState[0].title} )}/>
           </div>
           <div className="product-form__actions">
             <button type="submit">Add</button>
