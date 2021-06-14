0f1c44ad05db76d3b804768f6d98f32fa62ed9d2
alaz7622356694 <alireza.azari2235@outlook.com>
2021-06-15 02:39:31 +0430

    adding fetch

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -7,14 +7,25 @@ import ProductList from './ProductList'
 const Products=()=> {
   const [products,setProducts]=useState([])
   const addProductHandler=(item)=>{
+
+  fetch('https://my-json-server.typicode.com/typicode/demo/posts', {
+    method:'POST',
+    body:JSON.stringify(item),
+    headers:{'Content-Type':'application/json;charset=UTF-8'}
+  }).then((response)=> response.json().then((responseData)=>{
     setProducts((prevState)=>{
-return[
-  ...prevState , {
-    id: Math.random().toString(),
-    ...item
-  }
-]
-    })
+      return[
+        ...prevState , {
+          id: responseData.name,
+          ...item
+        }
+      ]
+          })
+  })
+  )
+
+
+
   }
   return (
     <div className="App">
