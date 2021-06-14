0eb4f31ed835317c803f7dcfabd2a476d9470a7f
alaz7622356694 <alireza.azari2235@outlook.com>
2021-06-14 16:37:33 +0430

    using arraye destructing for useState

--- src/components/Products/ProductForm.js
+++ src/components/Products/ProductForm.js
@@ -6,7 +6,7 @@ import './ProductForm.css'
 
 const ProductForm = React.memo((props) => {
 
-const inputState = useState({title:'' ,amount:''})
+const [inputState,setInputState] = useState({title:'' ,amount:''})
 
   const submitHandler = (event) => {
     event.preventDefault()
@@ -18,11 +18,11 @@ const inputState = useState({title:'' ,amount:''})
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Title</label>
-            <input type="text" id="title" value={inputState[0].title} onChange={(event)=>inputState[1]({title:event.target.value , amount:inputState[0].amount})}  />
+            <input type="text" id="title" value={inputState.title} onChange={(event)=>setInputState({title:event.target.value , amount:inputState.amount})}  />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" value={inputState[0].amount} onChange={(event)=>inputState[1]({amount:event.target.value , title:inputState[0].title} )}/>
+            <input type="number" id="amount" value={inputState.amount} onChange={(event)=>setInputState({amount:event.target.value , title:inputState.title} )}/>
           </div>
           <div className="product-form__actions">
             <button type="submit">Add</button>
